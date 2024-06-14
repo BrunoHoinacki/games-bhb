809836fee2f47ab4a4bae045836a3e9209d07f50
Bruno Hoinacki <brunohoinackib@gmail.com>
2024-06-14 20:27:05 -0300

    [ref] criando layout, melhorando SEO, adicionando logos

--- app.vue
+++ app.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
-  <NuxtPage />
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Joga-Aqui.com - Os Melhores Jogos Online para Crianças',
+  ogTitle: 'Joga-Aqui.com - Jogos Divertidos e Educativos para Crianças',
+  description: 'Descubra os melhores jogos online gratuitos para crianças no Joga-Aqui.com! Diversão garantida com jogos educativos, quebra-cabeças, aventuras e muito mais.',
+  ogDescription: 'No Joga-Aqui.com, você encontra uma variedade de jogos online gratuitos e seguros para crianças. Venha se divertir com jogos educativos, quebra-cabeças, aventuras e muito mais.',
+  ogImage: '/images/logo-fundo-azul.webp',
+  twitterCard: 'summary_large_image',
+  twitterTitle: 'Joga-Aqui.com - Jogos Online para Crianças',
+  twitterDescription: 'Jogue os melhores jogos online gratuitos para crianças no Joga-Aqui.com. Diversão e aprendizado garantidos!',
+  twitterImage: '/images/logo-fundo-azul.webp',
+  keywords: 'jogos online, jogos para crianças, jogos educativos, jogos gratuitos, diversão infantil, jogos seguros',
+  robots: 'index, follow',
+})
+</script>
