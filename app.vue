91d9a2c15d7a2ecff68c85ae4285e4ed73292035
Bruno Hoinacki <brunohoinackib@gmail.com>
2024-06-15 03:19:03 -0300

    [ref] formato logo

--- app.vue
+++ app.vue
@@ -25,11 +25,11 @@ useSeoMeta({
   ogTitle: 'Joga-Aqui.com - Jogos Divertidos e Educativos para Crianças',
   description: 'Descubra os melhores jogos online gratuitos para crianças no Joga-Aqui.com! Diversão garantida com jogos educativos, quebra-cabeças, aventuras e muito mais.',
   ogDescription: 'No Joga-Aqui.com, você encontra uma variedade de jogos online gratuitos e seguros para crianças. Venha se divertir com jogos educativos, quebra-cabeças, aventuras e muito mais.',
-  ogImage: '/images/logo-fundo-azul.webp',
+  ogImage: '/images/logo-fundo-azul.png',
   twitterCard: 'summary_large_image',
   twitterTitle: 'Joga-Aqui.com - Jogos Online para Crianças',
   twitterDescription: 'Jogue os melhores jogos online gratuitos para crianças no Joga-Aqui.com. Diversão e aprendizado garantidos!',
-  twitterImage: '/images/logo-fundo-azul.webp',
+  twitterImage: '/images/logo-fundo-azul.png',
   keywords: 'jogos online, jogos para crianças, jogos educativos, jogos gratuitos, diversão infantil, jogos seguros',
   robots: 'index, follow',
 })
