ca1c4453f4dbdd50276665caf981af6649a5ae02
Bruno Hoinacki <brunohoinackib@gmail.com>
2024-06-15 02:02:01 -0300

    [ref] menu icons

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,11 +1,16 @@
 <template>
-    <nav class="bg-teal-500 fixed w-full top-0 z-50 shadow-md">
-        <div class="container mx-auto flex justify-between items-center p-4">
-            <a href="/" class="text-white text-xl font-bold">Logo</a>
+    <nav class="bg-teal-500 fixed w-full top-0 z-50 shadow-md bg-opacity-60 backdrop-blur border-b border-gray-400">
+        <div class="container mx-auto flex justify-between items-center pt-2 px-4 pb-2">
+            <a href="/" class="text-white text-xl font-bold">
+                <img class="h-14" src="/public/images/logo-sem-fundo.png" alt="Logo">
+            </a>
             <ul class="flex space-x-4">
-                <li><a href="#" class="text-white hover:underline">Home</a></li>
-                <li><a href="#" class="text-white hover:underline">Games</a></li>
-                <li><a href="#" class="text-white hover:underline">Contact</a></li>
+                <li><a href="#" class="text-white hover:underline hidden md:inline">Home</a></li>
+                <li><a href="#" class="text-white hover:underline hidden md:inline">Games</a></li>
+                <li><a href="#" class="text-white hover:underline hidden md:inline">Contact</a></li>
+                <li><a href="#" class="text-white hover:underline md:hidden"><i class="fas fa-home"></i></a></li>
+                <li><a href="#" class="text-white hover:underline md:hidden"><i class="fas fa-layer-group"></i></a></li>
+                <li><a href="#" class="text-white hover:underline md:hidden"><i class="fas fa-headset"></i></a></li>
             </ul>
         </div>
     </nav>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,14 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
   app: {
-    pageTransition: { name: 'page', mode: 'out-in' }
-  },
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+        }
+      ]
+    }
+  }
 })
